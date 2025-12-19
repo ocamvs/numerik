7e2b134623fccc08178c21486e329a35d8cdbfbd
ocamvs <312danilgulaew@gmail.com>
2025-12-19 11:25:35 +0200

    1/2 config

---
+++ numerik/EquipmentService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using numerik.mod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numerik
+{
+    public class EquipmentService: DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Divisions>(entity =>
+            {
+                entity.
+            });
+        }
+    }
+}
--- numerik/mod/Equipment.cs
+++ numerik/mod/Equipment.cs
@@ -8,22 +8,51 @@ namespace numerik.mod
 {
     public class Divisions
     {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Supervisor { get; set; }
 
+        public List<Employee> Employees { get; set; }//
     }
-    public class Employees
+    public class Employee
     {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public string JobTitle { get; set; }
 
+        public int DivisionId { get; set; }
+        public Divisions Division { get; set; }
     }
     public class Equipment
     {
+        public int InventoryNumber { get; set; }
+        public int SerialNumber { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Supervisor { get; set; }
+        public DateTime InstallationDate { get; set; }
 
+        public int ResponsibleEmployeeId { get; set; }
+        public int TypeId { get; set; }
+        public Employee ResponsibleEmployee {  get; set; }
+        public EquipmentType Type { get; set; }
     }
     public class MovementHistory
     {
+        public DateTime MoveDate { get; set; }
 
+        public int LastEmId { get; set; }
+        public int FirstEmId { get; set; }
+        public int EquipmenId { get; set; }
+
+        public Employee LastEm {  get; set; }
+        public Employee FirstEm { get; set; }
+        public Equipment Equipmen { get; set; }
     }
     public class InstalledSoftware
     {
+        public DateTime InstallationDates { get; set; }
+
 
     }
 }
